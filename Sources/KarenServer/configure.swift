8ee52a33c11dd8b64abb0d1b4c522e05b51962bd
Dylan Dunn <dylandunn17@icloud.com>
2026-04-02 11:57:06 -0400

    Added database migration and routes for person slice

--- Sources/KarenServer/configure.swift
+++ Sources/KarenServer/configure.swift
@@ -12,12 +12,14 @@ public func configure(_ app: Application) async throws {
         hostname: Environment.get("DATABASE_HOST") ?? "192.168.1.156",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "karen",
-        password: Environment.get("DATABASE_PASSWORD") ?? "Password",
+        password: Environment.get("DATABASE_PASSWORD") ?? "PASSWORD",
         database: Environment.get("DATABASE_NAME") ?? "karen",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
     app.migrations.add(CreateLocations())
+    app.migrations.add(CreateMessages())
+    app.migrations.add(CreatePeople())
     
     try await app.autoRevert()
     try await app.autoMigrate()
--- Sources/KarenServer/routes.swift
+++ Sources/KarenServer/routes.swift
@@ -5,6 +5,7 @@ func routes(_ app: Application) throws {
     
     let locationController = LocationController()
     let chatController = ChatController()
+    let peopleController = PeopleController()
     
     //TODO: Create Route Collections
     
@@ -12,6 +13,11 @@ func routes(_ app: Application) throws {
     app.post("location", use: locationController.create)
     
     //Chat Routes
-    app.post("chat", use: chatController.send)
     app.get("chat", ":conversationID", use: chatController.getConversation)
+    app.post("chat", use: chatController.send)
+    
+    
+    //People Routes
+    app.get("person", ":personID", use: peopleController.getByID)
+    app.post("person", use: peopleController.create)
 }
